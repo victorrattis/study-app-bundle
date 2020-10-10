c772f8e2a02d1ad7b4ce8901f6245da4074a76b1
Victor Rattis <victorrattis@gmail.com>
2020-10-10 10:02:40 -0300

    Remove unsed codes

--- app/src/main/java/com/study/vhra/appbundle/MainActivity.kt
+++ app/src/main/java/com/study/vhra/appbundle/MainActivity.kt
@@ -1,42 +1,14 @@
 package com.study.vhra.appbundle
 
-import android.app.WallpaperInfo
-import android.content.Context
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.content.pm.ResolveInfo
-import android.content.pm.ServiceInfo
 import android.os.Bundle
-import android.util.Log
-import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        getWallpaperServices(this).forEach {
-            WallpaperInfo(this, it).also { info ->
-                findViewById<ImageView>(R.id.imageView)?.setImageDrawable(
-                    info.loadThumbnail(packageManager))
-                Log.d("devlog", "enabled: " + info.serviceInfo.enabled)
-                Log.d("devlog", "label: " + info.loadLabel(packageManager))
-            }
-            Log.d("devlog", "module: " + getServiceModuleName(it.serviceInfo))
-        }
+        findViewById<TextView>(R.id.text_app_version)?.text = BuildConfig.VERSION_NAME
     }
-
-    private fun getWallpaperServices(context: Context): MutableList<ResolveInfo> {
-        val intent = Intent("android.service.wallpaper.WallpaperService").apply {
-            this.`package` = context.packageName
-        }
-        return context.packageManager.queryIntentServices(
-            intent,
-            PackageManager.GET_META_DATA or PackageManager.MATCH_DISABLED_COMPONENTS)
-    }
-
-    private fun getServiceModuleName(serviceInfo: ServiceInfo): String? =
-        getString(serviceInfo.metaData.getInt("module"))
 }
--- contentB/src/main/java/com/vhra/contentb/ContentBService.kt
+++
@@ -1,9 +0,0 @@
-package com.vhra.contentb
-
-import android.service.wallpaper.WallpaperService
-
-class ContentBService : WallpaperService() {
-    override fun onCreateEngine(): Engine {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-}
